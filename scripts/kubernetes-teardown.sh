399f2ba1441c5f6d03429eef1a4cbad370827820
Fabian Lindfors <fabian@flapplabs.se>
2019-08-09 14:57:56 +0100

    Moved scripts to dev/scripts and samples to samples/

--- scripts/kubernetes-teardown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NAMESPACE=${1:-default}
-echo "Using namespace $NAMESPACE"
-
-kubectl delete -f scripts/kubernetes/service-broker.yaml --namespace $NAMESPACE
-kubectl delete -f scripts/kubernetes/auth-secret.yaml --namespace $NAMESPACE
-kubectl delete -f scripts/kubernetes/service.yaml --namespace $NAMESPACE
-kubectl delete -f scripts/kubernetes/deployment.yaml --namespace $NAMESPACE
