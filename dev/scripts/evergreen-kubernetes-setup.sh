fdb24f01b728e8995dceb638d957e6a4d7cc285c
Fabian Lindfors <fabian@flapplabs.se>
2019-08-14 17:41:03 +0100

    Changed K8S script to bash and added bash safety settings

--- dev/scripts/evergreen-kubernetes-setup.sh
+++ dev/scripts/evergreen-kubernetes-setup.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -eou pipefail
 
 echo "Install kubectl"
 curl -L -o bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
