656c89b35ee22d51118a32aaa47b45136db2ba9a
mrgunior <victor_w_ernet@hotmail.com>
2019-08-26 16:52:54 +0100

    Added conversion algorithm from YAML to JSON

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,10 +1,15 @@
 package e2e
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 	"testing"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
 	servicecatalog "github.com/kubernetes-sigs/service-catalog/pkg/client/clientset_generated/clientset"
@@ -233,23 +238,61 @@ func registerBroker(namespace string) error {
 	// Register broker with the service catalog. The URL points towards the
 	// internal DNS name of the broker service.
 	url := fmt.Sprintf("http://%s.%s", "atlas-service-broker", namespace)
-	_, err := svcatClient.ServicecatalogV1beta1().ServiceBrokers(namespace).Create(&v1beta1.ServiceBroker{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Spec: v1beta1.ServiceBrokerSpec{
-			CommonServiceBrokerSpec: v1beta1.CommonServiceBrokerSpec{
-				URL: url,
-			},
-			AuthInfo: &v1beta1.ServiceBrokerAuthInfo{
-				Basic: &v1beta1.BasicAuthConfig{
-					SecretRef: &v1beta1.LocalObjectReference{
-						Name: authSecretName,
-					},
-				},
-			},
-		},
-	})
+	serviceBroker := readInYAMLFile("../../samples/kubernetes/service-broker.yaml")
+	serviceBroker.ObjectMeta.Name = name
+	serviceBroker.Spec.CommonServiceBrokerSpec.URL = url
+	serviceBroker.Spec.AuthInfo.Basic.SecretRef.Name = authSecretName
+
+	_, err := svcatClient.ServicecatalogV1beta1().ServiceBrokers(namespace).Create(&serviceBroker)
 
 	return err
+}
+
+func readInYAMLFile(pathToYamlFile string) v1beta1.ServiceBroker {
+	// Read in the yaml file at the path given
+	yamlFile, err := ioutil.ReadFile(pathToYamlFile)
+	if err != nil {
+		log.Printf("Error while parsing YAML file %v, error: %s", pathToYamlFile, err)
+	}
+
+	// Map yamlFile to interface
+	var body interface{}
+	if err := yaml.Unmarshal([]byte(yamlFile), &body); err != nil {
+		panic(err)
+	}
+
+	// Convert yaml to its json counterpart
+	body = convertYAMLtoJSON(body)
+
+	// Generate json string from data structure
+	jsonFormat, err := json.Marshal(body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Map to service broker struct
+	serviceBroker := v1beta1.ServiceBroker{}
+	if err := json.Unmarshal(jsonFormat, &serviceBroker); err != nil {
+		panic(err)
+	}
+
+	return serviceBroker
+}
+
+func convertYAMLtoJSON(i interface{}) interface{} {
+	// Algorithm for conversion
+	switch item := i.(type) {
+	case map[interface{}]interface{}:
+		document := map[string]interface{}{}
+		for k, v := range item {
+			document[k.(string)] = convertYAMLtoJSON(v)
+		}
+		return document
+	case []interface{}:
+		for i, arr := range item {
+			item[i] = convertYAMLtoJSON(arr)
+		}
+	}
+
+	return i
 }
