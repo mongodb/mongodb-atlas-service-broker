586b4e4e7120bc7dfb2ef7e22a23fd38197bf6f1
mrgunior <victor_w_ernet@hotmail.com>
2019-08-27 16:21:57 +0100

    Created extra folder with crd for testing purposes

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -13,7 +13,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -134,110 +133,26 @@ func namespaceForTest(t *testing.T) string {
 
 // deployBroker will deploy the broker inside the specified namespace.
 func deployBroker(namespace string) error {
-	numOfReplicas := int32(1)
+	deploy := &appsv1.Deployment{}
+	testutil.ReadInYAMLFileAndConvert("../../samples/kubernetes/used_for_e2e_tests/deploy.yaml", &deploy)
+	_, err := kubeClient.AppsV1().Deployments(namespace).Create(deploy)
 
-	// Create deployment of the broker server.
-	_, err := kubeClient.AppsV1().Deployments(namespace).Create(&appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &numOfReplicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": name,
-				},
-			},
-			Template: v1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": name,
-					},
-				},
-				Spec: v1.PodSpec{
-					Containers: []v1.Container{
-						v1.Container{
-							Name:            name,
-							Image:           image,
-							ImagePullPolicy: "Never",
-							Ports: []v1.ContainerPort{
-								v1.ContainerPort{
-									ContainerPort: int32(4000),
-								},
-							},
-							Env: []v1.EnvVar{
-								v1.EnvVar{
-									Name:  "ATLAS_BASE_URL",
-									Value: atlasBaseURL,
-								},
-								v1.EnvVar{
-									Name:  "BROKER_HOST",
-									Value: "0.0.0.0",
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	// Create service to expose broker deployment internally.
-	_, err = kubeClient.CoreV1().Services(namespace).Create(&v1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-			Labels: map[string]string{
-				"app": name,
-			},
-		},
-		Spec: v1.ServiceSpec{
-			Selector: map[string]string{
-				"app": name,
-			},
-			Ports: []v1.ServicePort{
-				v1.ServicePort{
-					Protocol:   v1.ProtocolTCP,
-					Port:       80,
-					TargetPort: intstr.FromInt(4000),
-				},
-			},
-		},
-	})
+	exposeservice := &v1.Service{}
+	testutil.ReadInYAMLFileAndConvert("../../samples/kubernetes/used_for_e2e_tests/expose_service.yaml", &exposeservice)
+	_, err = kubeClient.CoreV1().Services(namespace).Create(exposeservice)
 
 	return err
 }
 
-// registerBroker will register the broker deployed in deployBroker with the
+// Create secret and registerBroker will register the broker deployed in deployBroker with the
 // service catalog.
 func registerBroker(namespace string) error {
-	authSecretName := name + "-auth"
-
-	username := atlasPublicKey + "@" + atlasGroupID
-	password := atlasPrivateKey
-
-	// Create secret to hold the basic auth credentials for the broker.
-	// The broker expects Atlas API credentials as part of the basic auth.
 	secret := &v1.Secret{}
 	testutil.ReadInYAMLFileAndConvert("../../samples/kubernetes/atlas-service-broker-auth.yaml", &secret)
 	kubeClient.CoreV1().Secrets(namespace).Create(secret)
-	secret.ObjectMeta.Name = authSecretName
-	secret.Data = map[string][]byte{
-		"username": []byte(username),
-		"password": []byte(password),
-	}
-
-	// Register broker with the service catalog. The URL points towards the
-	// internal DNS name of the broker service.
-	url := fmt.Sprintf("http://%s.%s", "atlas-service-broker", namespace)
 
 	servicebroker := v1beta1.ServiceBroker{}
 	testutil.ReadInYAMLFileAndConvert("../../samples/kubernetes/service-broker.yaml", &servicebroker)
-	servicebroker.ObjectMeta.Name = name
-	servicebroker.Spec.CommonServiceBrokerSpec.URL = url
-	servicebroker.Spec.AuthInfo.Basic.SecretRef.Name = authSecretName
 
 	_, err := svcatClient.ServicecatalogV1beta1().ServiceBrokers(namespace).Create(&servicebroker)
 
