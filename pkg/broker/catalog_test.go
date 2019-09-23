d20e4a0f42034eee216d475d6e51bd442e574347
Ben Elgar <me@benelgar.com>
2019-09-23 12:18:11 +0100

    Add whitelist unit test

--- pkg/broker/catalog_test.go
+++ pkg/broker/catalog_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
 )
 
 func TestCatalog(t *testing.T) {
@@ -17,4 +18,18 @@ func TestCatalog(t *testing.T) {
 	for _, service := range services {
 		assert.NotZerof(t, len(service.Plans), "Expected a non-zero amount of plans for service %s", service.Name)
 	}
+}
+
+func TestWhitelist(t *testing.T) {
+	_, _, ctx := setupTest()
+
+	logger := zap.S()
+	whitelist := Whitelist{}
+	whitelist["AWS"] = []string{"M10"}
+	broker := NewBrokerWithWhitelist(logger, whitelist)
+	services, err := broker.Services(ctx)
+
+	assert.Len(t, services, 1)
+	assert.Len(t, services[0].Plans, 1)
+	assert.NoError(t, err)
 }
